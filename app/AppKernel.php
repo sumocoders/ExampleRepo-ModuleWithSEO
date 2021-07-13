fff9ce0779b73680ad8d08b019430fd63d74dfa6
Bjorn Van Acker <bjorn@sumocoders.be>
2021-07-13 15:09:06 +0200

    Initial commit

---
+++ app/AppKernel.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ForkCMS\App;
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * The AppKernel provides a proper way to handle a request and transform it into a response.
+ */
+class AppKernel extends Kernel
+{
+    /**
+     * Load all the bundles we'll be using in our application.
+     */
+    public function registerBundles(): array
+    {
+        $bundles = [
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
+            new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new \ForkCMS\Bundle\InstallerBundle\ForkCMSInstallerBundle(),
+            new \ForkCMS\Bundle\CoreBundle\ForkCMSCoreBundle(),
+            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle(),
+            new \SimpleBus\SymfonyBridge\DoctrineOrmBridgeBundle(),
+            new \SimpleBus\SymfonyBridge\SimpleBusEventBusBundle(),
+            new \Backend\Modules\MediaLibrary\MediaLibrary(),
+            new \Backend\Modules\Mailmotor\Mailmotor(),
+            new \MailMotor\Bundle\MailMotorBundle\MailMotorMailMotorBundle(),
+            new \MailMotor\Bundle\MailChimpBundle\MailMotorMailChimpBundle(),
+            new \MailMotor\Bundle\CampaignMonitorBundle\MailMotorCampaignMonitorBundle(),
+            new \Liip\ImagineBundle\LiipImagineBundle(),
+        ];
+
+        if (in_array($this->getEnvironment(), ['dev', 'test'])) {
+            $bundles[] = new \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new \Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new \Matthias\SymfonyConsoleForm\Bundle\SymfonyConsoleFormBundle();
+            $bundles[] = new \ModuleGenerator\ModuleGeneratorBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $fs = new Filesystem();
+        if ($fs->exists(__DIR__ . '/config/parameters.yml')) {
+            $loader->load(__DIR__ . '/config/config.yml');
+        }
+
+        if ($fs->exists(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml')) {
+            $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
+        }
+    }
+}
